ba828d322ecc729455dbffb91ebcc106d35ef531
easonshen1413 <chs224@lehigh.edu>
2020-12-01 23:40:20 +0800

    Chenxiao_Shen

--- src/lehigh/MovingBalls.java
+++ src/lehigh/MovingBalls.java
@@ -2,15 +2,56 @@ package lehigh;
 
 import processing.core.PApplet;
 
+import java.util.Random;
+
 public class MovingBalls extends PApplet{
+
+
+    int numberOfBall = 6;
+    int [] x = new int[numberOfBall];
+    int [] y = new int[numberOfBall];
+    int [] speedX = new int[numberOfBall];
+    int [] speedY = new int[numberOfBall];
+
 
     public void settings(){
         size(500, 500);
+        Random random = new Random();
+        for(int i = 0; i < numberOfBall; i++){
+            x[i]=random.nextInt(500);
+            y[i]=random.nextInt(500);
+            speedX[i]=random.nextInt(5)+1;
+            speedY[i]=random.nextInt(5)+1;
+        }
     }
 
     public void draw(){
-        background(0);
-        ellipse(mouseX, mouseY, 20, 20);
+        background(255);
+        for (int i = 0; i < x.length;i++){
+            fill(0);
+            ellipse(x[i], y[i],50,50);
+
+            x[i] += speedX[i];
+            y[i] += speedY[i];
+
+            if(x[i]>500 || x[i]<0){
+                speedX[i] = -speedX[i];
+            }
+
+            if(y[i]>500 || y[i]<0) {
+                speedY[i] = -speedY[i];
+            }
+
+            for(int k = 0; k < numberOfBall; k++){
+                int Xd = x[k] - x[i];
+                int Yd = y[k] - y[i];
+                if(Math.abs(Xd) <= 100 && Math.abs(Yd) <= 100){
+                    line(x[k],y[k],x[i],y[i]);
+                }
+            }
+
+        }
+
     }
 
     public static void main(String[] args) {
@@ -19,3 +60,5 @@ public class MovingBalls extends PApplet{
         PApplet.runSketch(processingArgs, movingBalls);
     }
 }
+
+
---
+++ src/lehigh/MovingBalls2.java
@@ -0,0 +1,51 @@
+package lehigh;
+
+import processing.core.PApplet;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MovingBalls2 extends PApplet {
+    class Ball{
+        int x, y, radius;
+        float speedx, speedy;
+        float [] color;
+
+        Ball(){
+            radius = (int) random(5,28);
+            x = (int) random(width - radius/2);
+            y = (int) random(width - radius/2);
+            speedx = random(2.0f);
+            speedy = random(2.0f);
+            color = new float[] {random(1.0f),random(1.0f),random(1.0f)};
+        }
+
+        void drawTheBall(){
+
+        }
+    }
+
+    ArrayList<Ball> balls;
+
+    public void settings(){
+        size(400,400);
+        balls = new ArrayList<>();
+        for(int i = 0; i < 100; i++){
+            balls.add(new Ball());
+        }
+    }
+
+    public void draw(){
+        background(0);
+        fill(255);
+
+        for(Ball ball:balls){
+            ellipse(ball.x, ball.y, ball.radius, ball.radius);
+        }
+    }
+
+    public static void main(String[] args){
+        String[] processingArgs = {"MovingBalls2"};
+
+    }
+}
